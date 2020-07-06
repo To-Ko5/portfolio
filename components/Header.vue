31552bf9371b951d35cfdb1a35265207bb63c616
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2020-07-06 11:10:59 +0900

    Header追加

---
+++ components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app-bar dense collapse-on-scroll fixed dark>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+    <v-toolbar-title>Portfolio</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-menu left bottom>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn icon v-bind="attrs" v-on="on">
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item v-for="n in 5" :key="n" @click="() => {}">
+          <v-list-item-title>Option {{ n }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </v-app-bar>
+</template>
