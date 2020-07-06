55b320e8960ac7205969019cf5d7fb89f6333a13
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2020-07-06 11:11:39 +0900

    一覧ページ作成

---
+++ components/Item.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-card class="mx-auto" max-width="344">
+      <v-img
+        class="white--text align-end"
+        height="200px"
+        :src="work.fields.image.fields.file.url"
+      >
+      </v-img>
+      <v-card-title>{{ work.fields.title }}</v-card-title>
+      <v-card-subtitle>
+        {{ work.fields.subtitle }}
+      </v-card-subtitle>
+      <v-card-text>
+        <v-chip-group>
+          <v-chip>{{ work.fields.category.fields.name }}</v-chip>
+        </v-chip-group>
+        <v-spacer></v-spacer>
+        <v-divider></v-divider>
+        <v-chip-group>
+          <v-chip v-for="tag in work.fields.tag" :key="tag.sys.id">
+            {{ tag.fields.name }}
+          </v-chip>
+        </v-chip-group>
+      </v-card-text>
+    </v-card>
+  </v-layout>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    work: {
+      type: <any>{}
+    }
+  }
+})
+</script>
