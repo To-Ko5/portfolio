0e4e3c419a66a8f911de9334210c10263f2d2b23
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2020-07-07 10:26:10 +0900

    検索ページ追加

---
+++ components/SearchForm.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="search-form d-flex">
+    <v-text-field
+      v-model="searchWords"
+      outlined
+      clearable
+      label="Search"
+      type="text"
+    >
+      <template v-slot:append>
+        <v-fade-transition leave-absolute> </v-fade-transition>
+      </template>
+    </v-text-field>
+    <v-btn icon color="deep-orange" @click="sendSearch">
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      searchWords: ''
+    }
+  },
+  methods: {
+    sendSearch() {
+      this.$router.push('/search/' + this.searchWords)
+    }
+  }
+})
+</script>
+
+<style scoped>
+.search-form {
+  height: 100%;
+}
+</style>
