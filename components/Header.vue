f1026cc6d5b177a8d4d080199b65b393660cc39e
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2020-07-09 10:35:27 +0900

    Header,Sidebarのコンポーネント分割

--- components/Header.vue
+++ components/Header.vue
@@ -1,23 +1,54 @@
 <template>
-  <v-app-bar dense collapse-on-scroll fixed dark>
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  <div class="header">
+    <v-app-bar fixed dark>
+      <v-app-bar-nav-icon @click="drawerOpen"></v-app-bar-nav-icon>
 
-    <v-toolbar-title>Portfolio</v-toolbar-title>
+      <v-toolbar-title>Portfolio</v-toolbar-title>
 
-    <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
 
-    <v-menu left bottom>
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn icon v-bind="attrs" v-on="on">
-          <v-icon>mdi-dots-vertical</v-icon>
-        </v-btn>
-      </template>
+      <SearchForm />
 
-      <v-list>
-        <v-list-item v-for="n in 5" :key="n" @click="() => {}">
-          <v-list-item-title>Option {{ n }}</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-menu>
-  </v-app-bar>
+      <v-menu left bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+
+    <SideBar :drawer="drawer" />
+  </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import SearchForm from '~/components/SearchForm.vue'
+import SideBar from '~/components/SideBar.vue'
+
+export default Vue.extend({
+  name: 'Header',
+  components: {
+    SearchForm,
+    SideBar
+  },
+  data() {
+    return {
+      drawer: false,
+      group: 'group'
+    }
+  },
+  methods: {
+    drawerOpen() {
+      this.drawer = !this.drawer
+    }
+  }
+})
+</script>
---
+++ components/SideBar.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="side-bar">
+    <v-navigation-drawer v-model="drawerState" fixed temporary>
+      <v-list nav dense>
+        <div class="side-bar__search">
+          <SearchForm />
+        </div>
+        <v-list-item-group active-class="deep-purple--text text--accent-4">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import SearchForm from '~/components/SearchForm.vue'
+
+export default Vue.extend({
+  name: 'Sidebar',
+  components: {
+    SearchForm
+  },
+  props: {
+    drawer: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      drawerState: this.drawer
+    }
+  },
+  watch: {
+    drawer() {
+      this.drawerState = !this.drawerState
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.side-bar {
+  &__search {
+    margin-bottom: 8px;
+  }
+}
+</style>
