dceada57136ae6d1fd275c2e69ad510dac6f0a92
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2020-07-10 13:34:10 +0900

    検索結果の表示追加

--- pages/search/_keywords.vue
+++ pages/search/_keywords.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="search">
+    <div v-if="$route.params.keywords" class="search__result">
+      <p>検索キーワード:{{ $route.params.keywords }}</p>
+      <p>検索結果:{{ works.length }}</p>
+    </div>
+    <div v-if="works.length <= 0" class="not-keywords">
+      <v-btn outlined to="/">一覧へ戻る</v-btn>
+    </div>
     <Grid>
       <v-col
         cols="12"
@@ -19,7 +26,6 @@
 import Vue from 'vue'
 import Item from '~/components/Item.vue'
 import Grid from '~/components/Grid.vue'
-
 import { createClient } from '~/plugins/contentful.js'
 
 const client = createClient()
@@ -39,6 +45,10 @@ export default Vue.extend({
     ]).catch((error) => {
       console.log(error)
     })
+
+    if (response.items.length <= 0) {
+      console.log(response.items.length)
+    }
     return {
       works: response.items
     }
