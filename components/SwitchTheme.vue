3087ea7714c03ecd85e71cff8c18d87417944539
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2020-07-12 23:52:26 +0900

    テーマ切り替え機能追加

---
+++ components/SwitchTheme.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="switch-theme">
+    <v-switch
+      v-model="theme"
+      hide-details
+      color="white"
+      :prepend-icon="themeIcon"
+    ></v-switch>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      theme: true
+    }
+  },
+  computed: {
+    themeIcon(): string {
+      return this.theme ? 'mdi-weather-night' : 'mdi-weather-sunny'
+    }
+  },
+  watch: {
+    theme() {
+      // @ts-ignore
+      this.$vuetify.theme.dark = this.theme
+    }
+  }
+})
+</script>
+
+<style scoped></style>
