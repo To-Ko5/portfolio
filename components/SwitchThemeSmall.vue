8cc481e88c04456b54afe413ea39ae250ef0c245
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2020-07-13 23:31:20 +0900

    スマホ用テーマ切り替え機能追加

---
+++ components/SwitchThemeSmall.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="switch-theme-small">
+    <v-btn icon @click="switchTheme">
+      <v-icon>{{ themeIcon }}</v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      theme: true
+    }
+  },
+  computed: {
+    themeIcon(): string {
+      return this.theme ? 'mdi-weather-night' : 'mdi-weather-sunny'
+    }
+  },
+  watch: {
+    theme() {
+      // @ts-ignore
+      this.$vuetify.theme.dark = this.theme
+    }
+  },
+  methods: {
+    switchTheme() {
+      this.theme = !this.theme
+    }
+  }
+})
+</script>
+
+<style scoped></style>
