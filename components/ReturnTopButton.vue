2eac610852d28f687ff24b50df982d463b7bec38
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2020-07-19 23:17:51 +0900

    トップへ戻るボタン追加

---
+++ components/ReturnTopButton.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="return-top-button">
+    <transition name="button">
+      <v-btn v-show="buttonActive" fixed bottom right fab @click="returnTop">
+        <v-icon>mdi-chevron-up</v-icon>
+      </v-btn>
+    </transition>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      buttonActive: false,
+      scroll: 0
+    }
+  },
+  mounted() {
+    window.addEventListener('scroll', this.scrollWindow)
+  },
+  methods: {
+    returnTop() {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      })
+    },
+    scrollWindow() {
+      const top = 100
+      this.scroll = window.scrollY
+      if (top <= this.scroll) {
+        this.buttonActive = true
+      } else {
+        this.buttonActive = false
+      }
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.button-enter-active,
+.button-leave-active {
+  transition: opacity 0.5s;
+}
+.button-enter,
+.button-leave-to {
+  opacity: 0;
+}
+</style>
