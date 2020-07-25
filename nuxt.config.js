62df93bfe8308b48dbc49b34bae9fb50ede38b38
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2020-07-25 20:42:48 +0900

    headタグ修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,8 +14,8 @@ export default {
   },
 
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: '%s - ' + 'C-Portfolio',
+    title: 'C-Portfolio' || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
