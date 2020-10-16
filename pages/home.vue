e22b325c0a5b692cca0509997db716883f621d94
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2020-10-16 14:58:31 +0900

    Revert "Revert "リンク先パス変更""
    This reverts commit 5d92804c3884df3ea7857f210c5934c7e273eeaa.

---
+++ pages/home.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="home">
+    <Grid>
+      <v-col
+        cols="12"
+        sm="6"
+        md="4"
+        lg="4"
+        v-for="work in works"
+        :key="work.sys.id"
+      >
+        <Item :work="work" />
+      </v-col>
+    </Grid>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Item from '~/components/Item.vue'
+import Grid from '~/components/common/Grid.vue'
+
+import { createClient } from '~/plugins/contentful.js'
+const client = createClient()
+
+export default Vue.extend({
+  components: {
+    Item,
+    Grid
+  },
+  async asyncData() {
+    const [response]: any = await Promise.all([
+      client.getEntries({
+        content_type: 'work',
+        order: '-sys.createdAt'
+      })
+    ]).catch((error) => {
+      console.log(error)
+    })
+
+    return {
+      works: response.items
+    }
+  },
+  head() {
+    return {
+      title: 'Home'
+    }
+  }
+})
+</script>
