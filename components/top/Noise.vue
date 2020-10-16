7ec8d9e2c494c5c67d0b1f85e842c70927e68ea6
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2020-10-16 13:26:19 +0900

    トップページ追加

---
+++ components/top/Noise.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="noise"></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style lang="scss" scoped>
+.noise {
+  background: url('~@/assets/img/noise.gif') repeat left top;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 600;
+  height: 100%;
+  width: 100%;
+  opacity: 0.3;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,51 +1,180 @@
 <template>
   <div class="top-page">
-    <Grid>
-      <v-col
-        cols="12"
-        sm="6"
-        md="4"
-        lg="4"
-        v-for="work in works"
-        :key="work.sys.id"
-      >
-        <Item :work="work" />
-      </v-col>
-    </Grid>
+    <h1 class="title">C-Portfolio</h1>
+    <ul class="nav">
+      <li class="nav__list nav__list--home">
+        <nuxt-link to="/" @click.native="toLink('home')"> HOME</nuxt-link>
+      </li>
+      <li class="nav__list nav__list--about" @click="toLink('about')">
+        <nuxt-link to="/" @click.native="toLink('about')"> ABOUT</nuxt-link>
+      </li>
+      <li class="nav__list nav__list--gallery" @click="toLink('gallery')">
+        <nuxt-link to="/" @click.native="toLink('gallery')"> GALLERY</nuxt-link>
+      </li>
+      <li class="nav__list nav__list--contact" @click="toLink('contact')">
+        <nuxt-link to="/" @click.native="toLink('contact')"> CONTACT</nuxt-link>
+      </li>
+    </ul>
+    <transition name="fade">
+      <div v-if="transitionBlock" class="transition-block">
+        <p class="transition-block__words">{{ words | upperCase }}</p>
+      </div>
+    </transition>
+    <Glitch />
+    <Noise />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import Item from '~/components/Item.vue'
-import Grid from '~/components/common/Grid.vue'
-
-import { createClient } from '~/plugins/contentful.js'
-const client = createClient()
+import Glitch from '@/components/top/Glitch.vue'
+import Noise from '@/components/top/Noise.vue'
 
 export default Vue.extend({
+  layout: 'top-page',
   components: {
-    Item,
-    Grid
+    Glitch,
+    Noise
   },
-  async asyncData() {
-    const [response]: any = await Promise.all([
-      client.getEntries({
-        content_type: 'work',
-        order: '-sys.createdAt'
-      })
-    ]).catch((error) => {
-      console.log(error)
-    })
-
+  filters: {
+    upperCase(words: string) {
+      return words.toUpperCase()
+    }
+  },
+  data() {
     return {
-      works: response.items
+      words: '',
+      transitionBlock: false
+    }
+  },
+  methods: {
+    async toLink(link: string) {
+      this.words = link
+      this.transitionBlock = true
+      setTimeout(() => {
+        this.$router.push(link)
+      }, 500)
     }
   },
   head() {
     return {
-      title: 'Home'
+      titleTemplate: ''
     }
   }
 })
 </script>
+
+<style lang="scss" scoped>
+ul,
+li {
+  margin: 0;
+  padding: 0;
+  line-height: 1;
+}
+ul {
+  list-style: none;
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
+@supports (-webkit-touch-callout: none) {
+  .top-page {
+    /* The hack for Safari */
+    height: -webkit-fill-available;
+  }
+}
+.top-page {
+  min-height: 100vh;
+  width: 100%;
+  position: relative;
+  color: #fff;
+}
+.title {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  opacity: 0;
+  animation: fade-in 1s ease 0.5s forwards;
+  font-size: clamp(24px, calc(16px + 3vw), 80px) !important;
+  z-index: 900;
+}
+.nav {
+  width: 92%;
+  height: 90%;
+  position: absolute;
+  top: 5%;
+  left: 4%;
+  list-style: none;
+  opacity: 0;
+  animation: fade-in 3s ease 0.5s forwards;
+  z-index: 900;
+  &__list {
+    position: absolute;
+    font-size: 32px;
+    cursor: pointer;
+    @media screen and (max-width: 767px) {
+      font-size: 16px;
+    }
+    &--home {
+      top: 0;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+    &--about {
+      top: 50%;
+      left: 0;
+      transform: translateY(-50%);
+      writing-mode: vertical-rl;
+    }
+    &--gallery {
+      top: 50%;
+      right: 0;
+      transform: translateY(-50%);
+      writing-mode: vertical-rl;
+    }
+    &--contact {
+      bottom: 0;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+  }
+}
+
+@keyframes fade-in {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+.transition-block {
+  background: #212121;
+  height: 100vh;
+  width: 100%;
+  position: absolute;
+  z-index: 1000;
+  &__words {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: clamp(24px, calc(16px + 3vw), 80px);
+  }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.3s ease-out;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+  transform: scale(0.2);
+}
+</style>
