d190b3ff970ca70bba8904e85b9489ce04c7463e
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2020-10-16 22:25:32 +0900

    リンク追加

--- components/common/Footer.vue
+++ components/common/Footer.vue
@@ -1,7 +1,14 @@
 <template>
   <v-footer absolute>
     <v-spacer></v-spacer>
-    <div>&copy; C-Portfolio</div>
+    <div><nuxt-link to="/menu">&copy; C-Portfolio</nuxt-link></div>
     <v-spacer></v-spacer>
   </v-footer>
 </template>
+
+<style lang="scss" scoped>
+a {
+  text-decoration: none;
+  color: inherit !important;
+}
+</style>
